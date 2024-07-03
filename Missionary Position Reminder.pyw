ca4c2ad18b493e0685eaee4786aec1f867dc40a3
atrajit-sarkar <atrajit.sarkar@gmail.com>
2024-07-03 20:24:14 +0530

    Updated

---
+++ Missionary Position Reminder.pyw
@@ -0,0 +1,12 @@
+
+from plyer import notification
+import time
+time.sleep(0)
+notification.notify(
+app_name="Python",
+title = "Missionary Position Reminder",
+message ="You are a fucker.\n Always remember to fuck your wife, your gf, your stepsis regularly.\n Enjoy playboy life.",
+timeout =20,
+app_icon="D:\downloads\Python\Projects\PackageForAritra/WindowsNotification/icons/download.ico"
+)
+    
--- WindowsNotification/NotificationCustomise.py
+++ WindowsNotification/NotificationCustomise.py
@@ -1,21 +1,41 @@
+import os
+
 title=input("Enter your notification title: ")
 message=input("Enter your notification message: ")
 timeout=input("Enter your notification timeout: ")
 app_icon=input("Enter your notification app_icon: ")
 timesleep=input("Enter your notification timesleep: ")
+startup=input("Enter the time in sec when you want the notification shows after you open pc: ")
+onetime=input("Frequency: (onetime/repeatative)")
+if onetime=="repeatative":
 
-customnotification=f'''
+    customnotification=f'''
 from plyer import notification
 import time
-
+time.sleep({startup})
 while True:
     notification.notify(
+    app_name="Python",
     title = "{title}",
     message ="{message}",
     timeout ={timeout},
-    app_icon="{app_icon}"
+    app_icon="{os.getcwd()}/WindowsNotification/icons/{app_icon}"
     )
     time.sleep({timesleep})
-'''
+    '''
+elif onetime=="onetime":
+    customnotification=f'''
+from plyer import notification
+import time
+time.sleep({startup})
+notification.notify(
+app_name="Python",
+title = "{title}",
+message ="{message}",
+timeout ={timeout},
+app_icon="{os.getcwd()}/WindowsNotification/icons/{app_icon}"
+)
+    '''
+
 with open(f"{title}.pyw","w") as f:
     f.write(customnotification)
